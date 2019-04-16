320f8941f197813655d702b06b2d00b14e9fb3b7
vsmaksimov <vsmaksimov@inbox.ru>
2019-04-16 09:46:29 -0400

    add host

--- wcity/settings.py
+++ wcity/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')fnpmlhra9mk9w_x3d+ho68^u1fg1autemjzhflv&7z1%6*83@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
