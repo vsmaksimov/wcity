3fc862803d2e0b863253134dea70f5976e484aa8
Vasiliy <vsmaksimov@inbox.ru>
2019-04-16 21:19:36 +0300

    add shortcut render

--- fenster/views.py
+++ fenster/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('fenster/index.html')
-    return HttpResponse(template.render())
+    # template = loader.get_template('fenster/index.bck.html')
+    context = {
+        "window_height": 100,
+        "window_width": 50,
+        "input_id": "input_id",
+        "fenstertypes":[ True, True, False, None, ""]
+    }
+    # return HttpResponse(template.render(context))
+    return render(request, 'fenster/index.bck.html', context)
