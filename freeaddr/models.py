020f61f0f36c4145299175d025793e7639c95d94
vsmaksimov <vsmaksimov@inbox.ru>
2019-04-18 09:44:11 -0400

    add model freeaddr

--- freeaddr/models.py
+++ freeaddr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class FreeAddr(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    address = models.CharField(
+        default='',
+        max_length=1024
+    )
--- freeaddr/views.py
+++ freeaddr/views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from .models import FreeAddr
+from random import uniform
 
 
 def index(request):
-    free_addrs_list = []
-    free_addrs_list.append({'coords': '55.78 37.57', 'address': 'Moscow, Yamskogo polya street, 32'},)
-    free_addrs_list.append({'coords': '55.75 37.61', 'address': 'Moscow, Req Square, 3'}) 
-    free_addrs_list.append({'coords': '55.75 37.59', 'address': 'Moscow, New Arbat street, 8'})
+    # free_addrs_list = []
+    # free_addrs_list.append({'coords': '55.78 37.57', 'address': 'Moscow, Yamskogo polya street, 32'},)
+    # free_addrs_list.append({'coords': '55.75 37.61', 'address': 'Moscow, Req Square, 3'}) 
+    # free_addrs_list.append({'coords': '55.75 37.59', 'address': 'Moscow, New Arbat street, 8'})
+    
+    test_creation()
+    
+    free_addr_list = FreeAddr.objects.order_by("id")
 
     context = {
-        'free_addrs': free_addrs_list,
+        'free_addrs': free_addr_list,
     }
     return render(request, 'freeaddr/index.html', context)
+
+def test_creation():
+    free_addr = FreeAddr(
+        latitude=uniform(-90, 90),
+        longitude=uniform(-180, 180),
+        address = "address"
+    )
+    free_addr.save()
