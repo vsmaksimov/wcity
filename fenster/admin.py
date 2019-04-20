08fbb7ea1dccd732f9329f345185224f987fa031
vsmaksimov <vsmaksimov@inbox.ru>
2019-04-20 05:38:00 -0400

    add model purchase in admin.py

--- fenster/admin.py
+++ fenster/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Fenster
+from .models import Fenster, Purchase
 
 admin.site.register(Fenster)
+admin.site.register(Purchase)
