13cf2a15f0ae004c98c8dbd5869d445b6bf46a1c
vsmaksimov <vsmaksimov@inbox.ru>
2019-04-23 11:25:23 +0300

    add button 'clear filter'

--- fenster/urls.py
+++ fenster/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index',),
+    path('', views.index, name='index_fenster',),
     path('', views.index, name='buy'),
     path('sell/', views.sell, name='sell'),
     path('filter/', views.apply, name='apply')
